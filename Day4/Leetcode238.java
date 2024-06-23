4a80b23744dea0efb56c6df2ac00eba56c2c278d
Ved Sorathiya <130404833+VedSorathiya@users.noreply.github.com>
2024-06-23 19:27:29 +0530

    Update Leetcode238.java
    previous method was brute force, this has less time complexity

--- Day4/Leetcode238.java
+++ Day4/Leetcode238.java
@@ -4,20 +4,23 @@ import java.util.Arrays;
 
 public class Leetcode238 {
     public static int[] productExceptSelf(int[] nums) {
-        int arr[]=new int[nums.length];
-        int x=1;
-        for(int i=0;i<nums.length;i++){
-            for(int j=0;j<nums.length;j++){
-                if(i==j){
-                    continue;
-                }
-                x *= nums[j];
-            }
-            arr[i]=x;
+        int x=nums.length;
+        int left[] = new int[x];
+        int right[] = new int[x];
+        Arrays.fill(left,1);
+        Arrays.fill(right,1);
+        for(int i=1;i<x;++i){
+            left[i] = left[i-1]*nums[i-1];
+        }
+        for(int i=x-2;i>=0;--i){
+            right[i] = right[i+1]*nums[i+1];
+        }
+        int arr[] = new int[x];
+        for(int i=0;i<x;++i){
+            arr[i] = left[i]*right[i];
         }
         return arr;
     }
-
 //    public static void main(String args[]){
 //        //Test cases
 //        int a[] = {1,2,3,4};
